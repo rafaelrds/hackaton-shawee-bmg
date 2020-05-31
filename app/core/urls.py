9a7ec3abe7225f960dcab4e5b9b3bdc5fc5593d3
Caio Lopes <caiodesalopes@gmail.com>
2020-05-31 11:34:44 -0300

    start dashboard

--- app/core/urls.py
+++ app/core/urls.py
@@ -13,12 +13,16 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
+from dashboard import views as dash_views
 
 from quiz.views import test_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', test_view),
+    path('', dash_views.home),
 ]
