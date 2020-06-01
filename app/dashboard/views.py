a0da3324ade25b8f2a16aae6f21d55e3fb270d07
Caio Lopes <caiodesalopes@gmail.com>
2020-05-31 22:35:14 -0300

    fix errors

--- app/dashboard/views.py
+++ app/dashboard/views.py
@@ -4,9 +4,9 @@ from django.contrib.auth.decorators import login_required
 
 def dash_home(request):
     profile = getattr(request.user, 'profile', None)
+    if not profile:
+        return redirect('register')
     if profile and not profile.answered_quiz:
         return redirect('quiz')
-    else:
-        return redirect('register')
 
     return render(request, 'dashboard/home.html', {})
