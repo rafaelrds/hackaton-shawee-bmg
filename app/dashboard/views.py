6d6fcc2802b7876101b73bb7ceaa1833be882c20
Caio Lopes <caiodesalopes@gmail.com>
2020-05-31 23:16:22 -0300

    add landing

--- app/dashboard/views.py
+++ app/dashboard/views.py
@@ -5,7 +5,7 @@ from django.contrib.auth.decorators import login_required
 def dash_home(request):
     profile = getattr(request.user, 'profile', None)
     if not profile:
-        return redirect('register')
+        return render(request, 'dashboard/landing.html', {})
     if profile and not profile.answered_quiz:
         return redirect('quiz')
 
